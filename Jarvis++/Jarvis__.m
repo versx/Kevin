d8a49586e708c2b96311243e65fe2fcd3e05d38a
versx <versefx@gmail.com>
2020-02-25 18:39:48 -0800

    Initial commit

---
+++ Jarvis++/Jarvis__.m
@@ -0,0 +1,22 @@
+//
+//  Jarvis__.m
+//  Jarvis++
+//
+//  Created by versx on 2/24/20.
+//  Copyright (c) 2020 XYZ Software Solutions. All rights reserved.
+//
+
+#import "Jarvis__.h"
+
+@implementation Jarvis__
+
+-(id)init
+{
+	if ((self = [super init]))
+	{
+	}
+    
+	return self;
+}
+
+@end
